ff6b505d21bbcaae3f4f40d5fb1a359c70d1042c
Mari1548 <124880795+Mari1548@users.noreply.github.com>
2024-03-13 10:53:43 -0300

    Add files via upload

---
+++ Atv1/php/min.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>arquivo php</title>
+    <link rel="stylesheet" href="../css/estilo.css">
+</head>
+<body>
+
+
+
+    <?php
+  
+    $value = $_GET["value"];
+    $values = $_GET["values"];
+    $value_array = $_GET["value_array"];
+
+
+    echo "<p> o valor mínimo $value_array é: " . min($value, $values, $value_array);
+  
+
+    ?>
+</body>
+</html>
